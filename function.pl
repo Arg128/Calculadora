c86b15e4dbd168deaf7c355093ef461f28f0ed8f
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-10-31 20:36:46 -0500

    function.pl_0.3
    En aquí ahora se puede operar con comodidad, y como experimento se esta ya planificando un poco de como será su impresión en un html correctamente.

--- function.pl
+++ function.pl
@@ -20,12 +20,30 @@ print start_html('Resultado');
 ##                            $1                  $2      $3      $4      $5
 ##Este \s separa a $2 y a $4, ya que estos contienen espaciós, pero con \s este modifica y quita
 ##Dejando así una expresión solo con los numeros...
-while ($expresion =~ /(?:\()(\d+(\.\d+)?|(\.\d+))\s*([\+\-\*\/])\s*(\d+(\.\d+)?|\.\d+)(?:\))/g/) {
-  print <p1> El resultado es de $1 $3 $5 </p1>;
-}
+my $ecuacion = "( 2 * 2 )";
+while ($ecuacion =~ /(\()(\d+(\.\d+)?|\.\d+)\s*([\+\-\*\/])\s*(\d+(\.\d+)?|\.\d+)(\))/g) {
+    my $paren_abierto = $1;
+    my $num1 = $2;
+    my $op = $4;
+    my $num2 = $5;
+    my $paren_cerrado = $7;
 
-if ($expresion =~ /(?:\()(\d+|\.\d\d+)(?:\))/) {
-  print <p1> El resultado es de $1 </p1>;
+    # Detecta y manipula la expresión
+    my $resultado;
+    if ($op eq '+') {
+        $resultado = $num1 + $num2;
+    } elsif ($op eq '-') {
+        $resultado = $num1 - $num2;
+    } elsif ($op eq '*') {
+        $resultado = $num1 * $num2;
+    } elsif ($op eq '/') {
+        $resultado = $num1 / $num2;
+    }
+
+    # Reemplaza en la cadena original
+    $ecuacion =~ s/\Q$paren_abierto$num1\s*$op\s*$num2$paren_cerrado\E/($resultado)/;
+    print "<p> El resultado de $num1 $op $num2 es $resultado </p>";
+    print $ecuacion;
 }
 
 print end_html();
