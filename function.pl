001cbe693131ef773a871824a7822839e629dc22
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-10-31 19:39:03 -0500

    function.pl_0.2
    Versión donde se juega y se detecta ya los operadores de cada expresión a lo largo de la cadena indicada dcon la expresion '\g\

--- function.pl
+++ function.pl
@@ -13,6 +13,17 @@ my $expresion = $query --> param('ecuacion');
 print header(text/html);
 ##Titulo
 print start_html('Resultado');
+
+##Ahora se esta añadiendo una expreción, con ayuda de bucle para encontrar todas las posibles 
+##operaciónes, y detectar su formato de cambio [\+\-\*\/]
+
+##                            $1                  $2      $3      $4      $5
+##Este \s separa a $2 y a $4, ya que estos contienen espaciós, pero con \s este modifica y quita
+##Dejando así una expresión solo con los numeros...
+while ($expresion =~ /(?:\()(\d+(\.\d+)?|(\.\d+))\s*([\+\-\*\/])\s*(\d+(\.\d+)?|\.\d+)(?:\))/g/) {
+  print <p1> El resultado es de $1 $3 $5 </p1>;
+}
+
 if ($expresion =~ /(?:\()(\d+|\.\d\d+)(?:\))/) {
   print <p1> El resultado es de $1 </p1>;
 }
