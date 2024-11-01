a2308c825fcf6ad3f7f1d571ee824765ef671de9
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-10-31 22:41:08 -0500

    function.pl_final
    En aquí, se ha tratado mucho de enfatizar, sobre todo, en la indentación de demás paréntesis, por lo que, el 'while' de arriba primero busca las coincidencias de menor nivel, para después operarlas, y por consiguiente, saltar de nivel y seguir el programa como se debe.
    Se añadió $ecuacion =~ s/\(\s*\Q$expresion\E\s*\)/$resultado/;
    que funciona de maravilla, porque en ese punto estaba intentando repetir una, y otro, y otra expresión que actualizara la ecuación, ya que la mayoría o no actualizaba, o actualizaba parcialmente.

--- function.pl
+++ function.pl
@@ -6,36 +6,39 @@ use warnings;
 print "Content-type: text/html\n\n";
 print "<html><head><title>Resultado</title></head><body>";
 
-# Inicializa la ecuación directamente
-my $ecuacion = "(2+2) (8*6)";
+my $ecuacion = "(((5 * 5) * (6 * (8 + (7 / 1)))) + (( 6 * 5) - 29))";
 
-# Ahora se añade una expresión para encontrar todas las posibles operaciones
-while ($ecuacion =~ /(\(\s*(\d+(\.\d+)?|\.\d+)\s*([\+\-\*\/])\s*(\d+(\.\d+)?|\.\d+)\s*\))/g) {
-    my $expresion = $1; 
-    my $num1 = $2;
-    my $op = $4;
-    my $num2 = $5;
+# Evaluar todas las operaciones dentro de los paréntesis
+while ($ecuacion =~ /\(([^()]+)\)/g) {
+    my $expresion = $1;  # La expresión interna
+    # Busca los números y el operador
+    if ($expresion =~ /(\d+(\.\d+)?|\.\d+)\s*([\+\-\*\/])\s*(\d+(\.\d+)?|\.\d+)/) {
+        my $num1 = $1;       # Primer número
+        my $op = $3;         # Operador
+        my $num2 = $4;       # Segundo número
 
-    # Detecta y manipula la expresión
-    my $resultado;
-    if ($op eq '+') {
-        $resultado = $num1 + $num2;
-    } elsif ($op eq '-') {
-        $resultado = $num1 - $num2;
-    } elsif ($op eq '*') {
-        $resultado = $num1 * $num2;
-    } elsif ($op eq '/') {
-        $resultado = $num1 / $num2;
+        # Detecta y manipula la expresión
+        my $resultado;
+        if ($op eq '+') {
+            $resultado = $num1 + $num2;
+        } elsif ($op eq '-') {
+            $resultado = $num1 - $num2;
+        } elsif ($op eq '*') {
+            $resultado = $num1 * $num2;
+        } elsif ($op eq '/') {
+            $resultado = $num1 / $num2;
+        }
+
+        # Reemplaza en la cadena original
+        ##Expresion  servible
+        $ecuacion =~ s/\(\s*\Q$expresion\E\s*\)/$resultado/;
+        ##Expresion no servible
+        ##$ecuacion =~ s/\Q$expresion\E/$resultado/;
+        # Imprime el resultado
+        print "<p> El resultado de ($expresion) es $resultado </p>";
+        print "<p> Ecuación evaluada: $ecuacion </p>";
     }
-
-    # Reemplaza en la cadena original
-
-    $ecuacion =~ s/\Q$expresion\E/$resultado/;
-    print "<p> El resultado de $num1 $op $num2 es $resultado </p> $ecuacion";
-    print "<p> Ecuación evaluada: $ecuacion </p>"
 }
 
-# Imprime el resultado final
-print "<p> Ecuación evaluada: $ecuacion </p>";
+print "<p> Ecuación final evaluada: $ecuacion </p>";
 print "</body></html>";
-
